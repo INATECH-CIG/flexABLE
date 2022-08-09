27be86a2f034adddbb91710238cd6954334c356a
Nick Harder <nick.harder94@gmail.com>
2022-08-09 12:43:12 +0200

    v0.2.1 -moved execution into main file

--- flexABLE.py
+++ flexABLE.py
@@ -471,41 +471,4 @@ class World():
             
             logger.info("Merit Order calculated.")
             
-            
-# %%
-scenario = 'dsm_test'
-year = 2019
-days = 10
-
-importStorages = False
-importCRM = False
-importDHM = False
-importCBT = False
-checkAvailability = False
-meritOrder = True
-
-writeResultsToDB = False
-
-
-startingPoint = 0
-snapLength = 96*days    
-timeStamps = pd.date_range('{}-01-01T00:00:00'.format(year), '{}-01-01T00:00:00'.format(year+1), freq = '15T')
-
-env = World(snapLength,
-                simulationID = 'example',
-                startingDate = timeStamps[startingPoint],
-                writeResultsToDB = writeResultsToDB)
-
-
-env.loadScenario(scenario = scenario,
-                        checkAvailability=checkAvailability,
-                        importStorages=importStorages,
-                        importCRM=importCRM,
-                        importCBT=importCBT,
-                        importDHM=importDHM,
-                        meritOrder=meritOrder)
-
-# %%
-env.runSimulation()
-        
-        
+ 
